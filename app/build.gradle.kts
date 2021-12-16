5c2ead61aadfc99db4cb7230c503c5637657b1c5
lberrymage <lberrymage@tutanota.com>
2021-12-16 14:55:48 -0900

    Remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,9 @@ dependencies {
     val lifecycleVersion: String by rootProject.extra
 
     implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("com.google.android.material:material:1.4.0")
