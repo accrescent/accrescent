e12f47dc67bf78eb0e5bb432826435975a04220d
lberrymage <lberrymage@tutanota.com>
2022-01-08 19:28:06 -0900

    Bump version to 0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 31
         targetSdk = 31
         versionCode = 1
-        versionName = "0.0.0"
+        versionName = "0.1.0"
 
         vectorDrawables {
             useSupportLibrary = true
