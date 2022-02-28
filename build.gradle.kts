77126577d9a9acbdd67a3e44fb5a97f2f66a877b
lberrymage <lberrymage@tutanota.com>
2022-02-28 10:39:40 -0900

    Reorganize gradle files so dependabot can parse versions
    dependabot cannot check extra properties in Kotlin DSL
    (https://github.com/dependabot/dependabot-core/issues/3867), so move the
    versions to string literals for now.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
 buildscript {
-    val composeVersion by extra("1.0.5")
-    val hiltVersion by extra("2.40.5")
-    val lifecycleVersion by extra("2.4.0")
-    val roomVersion by extra("2.3.0")
-
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     }
 }
