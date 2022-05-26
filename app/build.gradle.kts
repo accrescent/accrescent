1ca1bf0d08448aeda10ae0872fbb62b042005a29
lberrymage <logan@lberrymage.dev>
2022-05-25 17:26:28 -0800

    Bump deps which depend on Kotlin 1.6.21
    - Bump Jetpack Compose from 1.1.1 to 1.2.0-beta02
    - Bump kotlinx-serialization-json from 1.3.2 to 1.3.3
    - Bump kotlinx.serialization from 1.6.10 to 1.6.21
    - Bump kotlin gradle plugin from 1.6.10 to 1.6.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("dagger.hilt.android.plugin")
     id("kotlin-android")
     id("kotlin-kapt")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
 }
 
 val keystorePropertiesFile = rootProject.file("keystore.properties")
@@ -73,7 +73,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.1"
+        kotlinCompilerExtensionVersion = "1.2.0-beta02"
     }
     packagingOptions {
         resources {
@@ -84,9 +84,9 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.compose.material:material:1.1.1")
-    implementation("androidx.compose.ui:ui:1.1.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
+    implementation("androidx.compose.material:material:1.2.0-beta02")
+    implementation("androidx.compose.ui:ui:1.2.0-beta02")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0-beta02")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.hilt:hilt-work:1.0.0")
     implementation("androidx.lifecycle:lifecycle-process:2.4.1")
@@ -99,8 +99,8 @@ dependencies {
     implementation("com.google.android.material:material:1.6.0")
     implementation("com.google.dagger:hilt-android:2.41")
     implementation("org.bouncycastle:bcprov-jdk15to18:1.70")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.2.0-beta02")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
     kapt("androidx.room:room-compiler:2.4.2")
     kapt("com.google.dagger:hilt-android-compiler:2.41")
