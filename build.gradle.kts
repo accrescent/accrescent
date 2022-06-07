5283e5507ac346722882acf57d5ef9ad56138b43
lberrymage <logan@lberrymage.dev>
2022-06-07 02:20:34 -0800

    Work around broken dependabot handling of repo declarations
    https://github.com/dependabot/dependabot-core/issues/3901

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 buildscript {
     repositories {
-        google()
-        mavenCentral()
+        // We declare repositories this way because dependabot doesn't pick up all our dependencies
+        // otherwise. Relevant issue: https://github.com/dependabot/dependabot-core/issues/3901.
+        maven { url = uri("https://dl.google.com/dl/android/maven2")} // google()
+        maven { url = uri("https://repo.maven.apache.org/maven2")} // mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
