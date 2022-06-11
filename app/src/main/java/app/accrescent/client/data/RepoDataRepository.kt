bd37eca0075724ea6c9f08adafb2b16b64d4672c
lberrymage <logan@lberrymage.dev>
2022-06-10 16:10:56 -0800

    Use string resource for error in RepoDataRepository
    This was meant to be used in 142481fbe942d94ee5584cfb142a7626dbc9ef14
    but was missed.

--- app/src/main/java/app/accrescent/client/data/RepoDataRepository.kt
+++ app/src/main/java/app/accrescent/client/data/RepoDataRepository.kt
@@ -1,17 +1,21 @@
 package app.accrescent.client.data
 
+import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.longPreferencesKey
+import app.accrescent.client.R
 import app.accrescent.client.data.db.App
 import app.accrescent.client.data.db.SigningCert
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import java.security.GeneralSecurityException
 import javax.inject.Inject
 
 class RepoDataRepository @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val repoDataRemoteDataSource: RepoDataRemoteDataSource,
     private val repoDataLocalDataSource: RepoDataLocalDataSource,
     private val timestampDataStore: DataStore<Preferences>,
@@ -24,7 +28,7 @@ class RepoDataRepository @Inject constructor(
         if (repoData.timestamp >= storedTimestamp) {
             timestampDataStore.edit { it[timestampKey] = repoData.timestamp }
         } else {
-            throw GeneralSecurityException("repodata timestamp less than saved value")
+            throw GeneralSecurityException(context.getString(R.string.repodata_timestamp_less))
         }
 
         // This approach is suboptimal since we're replacing existing app entries instead of
