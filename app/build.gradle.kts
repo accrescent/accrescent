10da8f8711928101157cb1351d334a82a408ab0a
lberrymage <logan@lberrymage.dev>
2022-06-29 17:18:31 -0800

    Remove unused compose and lifecycle deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,16 +84,13 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.4.0")
     implementation("androidx.compose.material:material:1.2.0-rc03")
     implementation("androidx.compose.material:material-icons-extended:1.2.0-rc03")
     implementation("androidx.compose.ui:ui:1.2.0-rc03")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0-rc03")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("androidx.hilt:hilt-work:1.0.0")
     implementation("androidx.lifecycle:lifecycle-process:2.5.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0")
     implementation("androidx.navigation:navigation-compose:2.5.0")
     implementation("androidx.room:room-ktx:2.4.2")
@@ -105,7 +102,6 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.42")
     implementation("org.bouncycastle:bcprov-jdk18on:1.71")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.0-rc03")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
     kapt("androidx.room:room-compiler:2.4.2")
     kapt("com.google.dagger:hilt-android-compiler:2.42")
