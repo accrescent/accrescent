c7dad2086bf1e284d7c0610b44e54fdb938ab3cd
lberrymage <logan@lberrymage.dev>
2022-07-29 18:14:17 -0800

    Bump material3 from 1.0.0-alpha14 to 1.0.0-alpha15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ android {
 dependencies {
     implementation("androidx.compose.material:material:1.2.0")
     implementation("androidx.compose.material:material-icons-extended:1.2.0")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha14")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha15")
     implementation("androidx.compose.ui:ui:1.2.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
