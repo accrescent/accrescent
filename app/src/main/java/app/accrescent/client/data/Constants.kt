862c353c90cd429dc472ebcdd3ae0619990ce52e
lberrymage <logan@lberrymage.dev>
2022-07-31 19:57:34 -0800

    Bump MIN_TIMESTAMP

--- app/src/main/java/app/accrescent/client/data/Constants.kt
+++ app/src/main/java/app/accrescent/client/data/Constants.kt
@@ -5,4 +5,4 @@ const val REPOSITORY_URL = "https://store.${ROOT_DOMAIN}"
 
 const val REPODATA_PUBKEY = "RWT8aZ/NdUmXCPqQ0we7UyCe34q1xRfncBFVK5dI3ok9BkL1bFF3mgh3"
 const val PUBKEY_VERSION = 0
-const val MIN_TIMESTAMP: Long = 1658117448
+const val MIN_TIMESTAMP: Long = 1659326187
