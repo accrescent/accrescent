accrescent/accrescent

commit 74ac8850dd1746a4c510e9e4f1d6511e4042ba37

Align app card lists against top of screen

app/src/main/java/app/accrescent/client/ui/AppListScreen.kt
@@ -1,9 +1,7 @@
 package app.accrescent.client.ui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -39,10 +37,10 @@ fun AppListScreen(
         state = rememberSwipeRefreshState(viewModel.isRefreshing),
         onRefresh = { viewModel.refreshRepoData() },
     ) {
-        LazyColumn(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-            if (apps.isEmpty()) {
-                item { CenteredText(stringResource(R.string.swipe_refresh)) }
-            } else {
+        if (apps.isEmpty()) {
+            CenteredText(stringResource(R.string.swipe_refresh))
+        } else {
+            LazyColumn {
                 item { Spacer(Modifier.height(16.dp)) }
                 items(apps, key = { app -> app.id }) { app ->
                     InstallableAppCard(

app/src/main/java/app/accrescent/client/ui/AppUpdatesScreen.kt
@@ -1,9 +1,7 @@
 package app.accrescent.client.ui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -43,12 +41,12 @@ fun AppUpdatesScreen(
             viewModel.refreshInstallStatuses()
         },
     ) {
-        LazyColumn(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-            if (apps.isEmpty()) {
-                item { CenteredText(stringResource(R.string.swipe_refresh)) }
-            } else if (updatableApps.isEmpty()) {
-                item { CenteredText(stringResource(R.string.up_to_date)) }
-            } else {
+        if (apps.isEmpty()) {
+            CenteredText(stringResource(R.string.swipe_refresh))
+        } else if (updatableApps.isEmpty()) {
+            CenteredText(stringResource(R.string.up_to_date))
+        } else {
+            LazyColumn {
                 item { Spacer(Modifier.height(16.dp)) }
                 items(updatableApps, key = { app -> app.id }) { app ->
                     InstallableAppCard(
@@ -62,12 +60,12 @@ fun AppUpdatesScreen(
                 }
             }
         }
+    }
 
-        if (viewModel.error != null) {
-            LaunchedEffect(scaffoldState.snackbarHostState) {
-                scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
-                viewModel.error = null
-            }
+    if (viewModel.error != null) {
+        LaunchedEffect(scaffoldState.snackbarHostState) {
+            scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
+            viewModel.error = null
         }
     }
 }

app/src/main/java/app/accrescent/client/ui/InstalledAppsScreen.kt
@@ -1,9 +1,7 @@
 package app.accrescent.client.ui
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -48,12 +46,12 @@ fun InstalledAppsScreen(
             viewModel.refreshInstallStatuses()
         },
     ) {
-        LazyColumn(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-            if (apps.isEmpty()) {
-                item { CenteredText(stringResource(R.string.swipe_refresh)) }
-            } else if (installedApps.isEmpty()) {
-                item { CenteredText(stringResource(R.string.no_apps_installed)) }
-            } else {
+        if (apps.isEmpty()) {
+            CenteredText(stringResource(R.string.swipe_refresh))
+        } else if (installedApps.isEmpty()) {
+            CenteredText(stringResource(R.string.no_apps_installed))
+        } else {
+            LazyColumn {
                 item { Spacer(Modifier.height(16.dp)) }
                 items(installedApps, key = { app -> app.id }) { app ->
                     InstallableAppCard(
@@ -67,12 +65,12 @@ fun InstalledAppsScreen(
                 }
             }
         }
+    }
 
-        if (viewModel.error != null) {
-            LaunchedEffect(scaffoldState.snackbarHostState) {
-                scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
-                viewModel.error = null
-            }
+    if (viewModel.error != null) {
+        LaunchedEffect(scaffoldState.snackbarHostState) {
+            scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
+            viewModel.error = null
         }
     }
 }

app/src/main/java/app/accrescent/client/ui/CenteredText.kt
@@ -1,12 +1,16 @@
 package app.accrescent.client.ui
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun CenteredText(message: String) {
-    Text(text = message, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = message, textAlign = TextAlign.Center)
+    }
 }

commit ccaa7fedab9f9f417f836bde52533953e9f077bd

Fix swipe refrseh indicator not functioning
This commit partially reverts 74ac885 which caused the regression.

app/src/main/java/app/accrescent/client/ui/AppListScreen.kt
@@ -1,7 +1,9 @@
 package app.accrescent.client.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -37,10 +39,12 @@ fun AppListScreen(
         state = rememberSwipeRefreshState(viewModel.isRefreshing),
         onRefresh = { viewModel.refreshRepoData() },
     ) {
-        if (apps.isEmpty()) {
-            CenteredText(stringResource(R.string.swipe_refresh))
-        } else {
-            LazyColumn {
+        val verticalArrangement = if (apps.isEmpty()) Arrangement.Center else Arrangement.Top
+
+        LazyColumn(Modifier.fillMaxSize(), verticalArrangement = verticalArrangement) {
+            if (apps.isEmpty()) {
+                item { CenteredText(stringResource(R.string.swipe_refresh)) }
+            } else {
                 item { Spacer(Modifier.height(16.dp)) }
                 items(apps, key = { app -> app.id }) { app ->
                     InstallableAppCard(

app/src/main/java/app/accrescent/client/ui/AppUpdatesScreen.kt
@@ -1,7 +1,9 @@
 package app.accrescent.client.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -41,12 +43,15 @@ fun AppUpdatesScreen(
             viewModel.refreshInstallStatuses()
         },
     ) {
-        if (apps.isEmpty()) {
-            CenteredText(stringResource(R.string.swipe_refresh))
-        } else if (updatableApps.isEmpty()) {
-            CenteredText(stringResource(R.string.up_to_date))
-        } else {
-            LazyColumn {
+        val verticalArrangement =
+            if (apps.isEmpty() || updatableApps.isEmpty()) Arrangement.Center else Arrangement.Top
+
+        LazyColumn(Modifier.fillMaxSize(), verticalArrangement = verticalArrangement) {
+            if (apps.isEmpty()) {
+                item { CenteredText(stringResource(R.string.swipe_refresh)) }
+            } else if (updatableApps.isEmpty()) {
+                item { CenteredText(stringResource(R.string.up_to_date)) }
+            } else {
                 item { Spacer(Modifier.height(16.dp)) }
                 items(updatableApps, key = { app -> app.id }) { app ->
                     InstallableAppCard(
@@ -60,12 +65,12 @@ fun AppUpdatesScreen(
                 }
             }
         }
-    }
 
-    if (viewModel.error != null) {
-        LaunchedEffect(scaffoldState.snackbarHostState) {
-            scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
-            viewModel.error = null
+        if (viewModel.error != null) {
+            LaunchedEffect(scaffoldState.snackbarHostState) {
+                scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
+                viewModel.error = null
+            }
         }
     }
 }

app/src/main/java/app/accrescent/client/ui/InstalledAppsScreen.kt
@@ -1,7 +1,9 @@
 package app.accrescent.client.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -46,12 +48,15 @@ fun InstalledAppsScreen(
             viewModel.refreshInstallStatuses()
         },
     ) {
-        if (apps.isEmpty()) {
-            CenteredText(stringResource(R.string.swipe_refresh))
-        } else if (installedApps.isEmpty()) {
-            CenteredText(stringResource(R.string.no_apps_installed))
-        } else {
-            LazyColumn {
+        val verticalArrangement =
+            if (apps.isEmpty() || installedApps.isEmpty()) Arrangement.Center else Arrangement.Top
+
+        LazyColumn(Modifier.fillMaxSize(), verticalArrangement = verticalArrangement) {
+            if (apps.isEmpty()) {
+                item { CenteredText(stringResource(R.string.swipe_refresh)) }
+            } else if (installedApps.isEmpty()) {
+                item { CenteredText(stringResource(R.string.no_apps_installed)) }
+            } else {
                 item { Spacer(Modifier.height(16.dp)) }
                 items(installedApps, key = { app -> app.id }) { app ->
                     InstallableAppCard(
@@ -65,12 +70,12 @@ fun InstalledAppsScreen(
                 }
             }
         }
-    }
 
-    if (viewModel.error != null) {
-        LaunchedEffect(scaffoldState.snackbarHostState) {
-            scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
-            viewModel.error = null
+        if (viewModel.error != null) {
+            LaunchedEffect(scaffoldState.snackbarHostState) {
+                scaffoldState.snackbarHostState.showSnackbar(message = viewModel.error!!)
+                viewModel.error = null
+            }
         }
     }
 }