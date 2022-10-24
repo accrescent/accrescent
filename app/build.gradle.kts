74fd70f19b10cfe99b464efcc3fc0d5b87b2eec3
lberrymage <logan@lberrymage.dev>
2022-10-24 12:39:55 -0800

    Bump Compose UI from 1.2.1 to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,7 +116,7 @@ android {
 dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.3.0")
     implementation("androidx.compose.material3:material3:1.0.0")
-    implementation("androidx.compose.ui:ui:1.2.1")
+    implementation("androidx.compose.ui:ui:1.3.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation("androidx.hilt:hilt-work:1.0.0")
