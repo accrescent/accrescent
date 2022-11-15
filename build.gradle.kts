e2d8e78f1e8fc53ca96eed3bcc1ad10dae75d4d3
lberrymage <logan@lberrymage.dev>
2022-11-14 21:36:41 -0900

    Bump Hilt from 2.44.1 to 2.44.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
