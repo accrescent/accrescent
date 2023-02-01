77a781698b881362a5b4be16859f9093bbba3383
lberrymage <logan@lberrymage.dev>
2023-02-01 12:38:03 -0700

    Bump AGP from 7.4.0 to 7.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.0")
+        classpath("com.android.tools.build:gradle:7.4.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     }
