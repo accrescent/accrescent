fe04d4355732763eb5767d9d22611e05ca68d9be
lberrymage <logan@lberrymage.dev>
2023-02-28 22:30:59 -0700

    Implement icon image loading
    Currently no verification is done of the images against their sha256
    hashes in the signed repository metadata.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,6 +147,7 @@ dependencies {
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
     implementation("com.google.android.material:material:1.8.0")
     implementation("com.google.dagger:hilt-android:2.45")
+    implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("org.bouncycastle:bcprov-jdk18on:1.72")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
--- app/src/main/java/app/accrescent/client/ui/AppCard.kt
+++ app/src/main/java/app/accrescent/client/ui/AppCard.kt
@@ -1,18 +1,22 @@
 package app.accrescent.client.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import app.accrescent.client.data.REPOSITORY_URL
 import app.accrescent.client.data.db.App
+import coil.compose.AsyncImage
 
 @Composable
 fun AppCard(app: App, onClick: () -> Unit) {
@@ -22,14 +26,18 @@ fun AppCard(app: App, onClick: () -> Unit) {
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .clickable { onClick() },
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            AsyncImage(
+                "$REPOSITORY_URL/apps/${app.id}/icon.png",
+                "App icon",
+                Modifier
+                    .padding(horizontal = 12.dp, vertical = 8.dp)
+                    .size(48.dp)
+                    .clip(CircleShape),
+            )
             Text(
                 app.name,
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                modifier = Modifier.padding(vertical = 24.dp),
                 fontWeight = FontWeight.Medium,
             )
         }
