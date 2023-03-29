11240f1c4c69a6ee4a486e147b0d4e02f19a716c
lberrymage <logan@lberrymage.dev>
2023-03-28 21:15:45 -0700

    Show loading indicator for app icons when appropriate

--- app/src/main/java/app/accrescent/client/ui/AppIcon.kt
+++ app/src/main/java/app/accrescent/client/ui/AppIcon.kt
@@ -1,17 +1,19 @@
 package app.accrescent.client.ui
 
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import app.accrescent.client.data.REPOSITORY_URL
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 
 @Composable
 fun AppIcon(appId: String, modifier: Modifier = Modifier) {
-    AsyncImage(
+    SubcomposeAsyncImage(
         "$REPOSITORY_URL/apps/$appId/icon.png",
         "App icon",
         modifier.clip(CircleShape),
+        loading = { CircularProgressIndicator() },
     )
 }
