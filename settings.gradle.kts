15c7d2761d5959afccc498632683899fdc28980d
Patryk Mis <foss@patrykmis.com>
2023-04-19 22:17:54 +0200

    Update Gradle build scripts
    Signed-off-by: Patryk Mis <foss@patrykmis.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 rootProject.name = "Accrescent"
