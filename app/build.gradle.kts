47388a59973af61326932d02d0b8699ce9bfdd2b
lberrymage <logan@lberrymage.dev>
2023-05-03 21:43:24 -0700

    Bump Compose BOM from 2023.04.01 to 2023.05.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,7 +136,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.05.00"))
     implementation("androidx.compose.material:material")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
