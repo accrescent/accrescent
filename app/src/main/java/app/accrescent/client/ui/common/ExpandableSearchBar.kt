0ffe43692d54218fb4ce23899fdd71f65e1a555f
lberrymage <logan@lberrymage.dev>
2023-05-10 17:04:00 -0700

    Remove no-longer-necessary experimental @OptIn
    OutlinedTextField is no longer experimental in Compose Material 3 1.1.0.

--- app/src/main/java/app/accrescent/client/ui/common/ExpandableSearchBar.kt
+++ app/src/main/java/app/accrescent/client/ui/common/ExpandableSearchBar.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -71,7 +70,6 @@ fun CollapsedSearchView(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExpandedSearchView(
     textFieldValue: MutableState<TextFieldValue>,
