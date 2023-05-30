c66e214578528cf876c146a8f876026a17622d06
lberrymage <logan@lberrymage.dev>
2023-05-29 22:15:09 -0700

    Migrate to using a version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    id("app.accrescent.tools.bundletool") version "0.1.2" apply false
-    id("com.android.application") version "8.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.46.1" apply false
-    id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21" apply false
+    alias(libs.plugins.accrescent.bundletool) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.dagger) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
 
 tasks.register("clean", Delete::class) {
