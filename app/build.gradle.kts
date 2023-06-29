fb13d88d648c44fbe02c73031cc176cb7a5ca5f3
lberrymage <logan@lberrymage.dev>
2023-06-28 22:41:01 -0700

    Bump Compose compiler to 1.4.8 / Kotlin to 1.8.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
 
     dependenciesInfo {
