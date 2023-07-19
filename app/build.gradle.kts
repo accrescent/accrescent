480b4846c6b871e7d9c25a4cce536d4b61effe68
Patryk Miś <foss@patrykmis.com>
2023-07-19 23:42:59 +0200

    Full update to Kotlin v1.9.0
    Signed-off-by: Patryk Miś <foss@patrykmis.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.8"
+        kotlinCompilerExtensionVersion = "1.5.0"
     }
 
     dependenciesInfo {
