f9aa083aa31e25ed9de872e28f1293f7e23b922f
Patryk Miś <foss@patrykmis.com>
2023-07-19 23:44:59 +0200

    Drop deleting rootProject.buildDir as it's no longer needed
    Signed-off-by: Patryk Miś <foss@patrykmis.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,3 @@ plugins {
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
