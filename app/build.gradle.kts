8d60bb3140e50012a43c26a0258072c5b613570e
Patryk Miś <foss@patrykmis.com>
2023-08-21 02:39:53 +0200

    Bump dependencies
    Signed-off-by: Patryk Miś <foss@patrykmis.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 
     dependenciesInfo {
