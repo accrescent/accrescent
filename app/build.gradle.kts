913763897442e4338d9f0de043ed564d71aff4d2
lberrymage <logan@lberrymage.dev>
2023-11-07 17:17:34 -0700

    Bump Compose compiler to 1.5.4 / Kotlin to 1.9.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,7 +112,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 
     dependenciesInfo {
