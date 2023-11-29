bed1faf38dab99ec452cbce4263ae9fba8e0980f
lberrymage <logan@lberrymage.dev>
2023-11-29 15:00:36 -0700

    Bump Compose compiler from 1.5.4 to 1.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,7 +110,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
 
     dependenciesInfo {
