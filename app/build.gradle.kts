6d68bba0f631c806ca8e1d9b6df2c0a6358e1c23
Logan Magee <logan@lberrymage.dev>
2024-03-19 15:49:19 -0700

    Bump version to 0.18.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "app.accrescent.client"
         minSdk = 29
         targetSdk = 34
-        versionCode = 41
-        versionName = "0.17.1"
+        versionCode = 42
+        versionName = "0.18.0"
         resourceConfigurations.addAll(listOf(
             "ar",
             "az",
