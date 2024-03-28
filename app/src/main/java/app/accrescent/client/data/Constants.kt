c582d187474eef8f67710720bca14e0fce505ef9
lberrymage <46338569+lberrymage@users.noreply.github.com>
2024-03-28 10:08:33 +0000

    Bump MIN_TIMESTAMP

--- app/src/main/java/app/accrescent/client/data/Constants.kt
+++ app/src/main/java/app/accrescent/client/data/Constants.kt
@@ -6,4 +6,4 @@ const val SOURCE_CODE_URL = "https://github.com/accrescent/accrescent"
 
 const val REPODATA_PUBKEY = "RWT8aZ/NdUmXCPqQ0we7UyCe34q1xRfncBFVK5dI3ok9BkL1bFF3mgh3"
 const val PUBKEY_VERSION = 0
-const val MIN_TIMESTAMP: Long = 1708379041
+const val MIN_TIMESTAMP: Long = 1711584826
