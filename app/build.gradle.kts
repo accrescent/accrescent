ea947eca5901a5ae25d039a913a8bb28d3b51fbe
lberrymage <logan@lberrymage.dev>
2025-06-20 19:37:47 +0000

    Bump target SDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ android {
     defaultConfig {
         applicationId = "app.accrescent.client"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 49
         versionName = "0.25.0"
     }
